6934cd04eb64ef33fb16dc0d92d24f317a652baa
Joe Zucco <josephzucco@gmail.com>
2021-08-08 21:28:26 -0400

    solution

--- coinchange.js
+++ coinchange.js
@@ -1,4 +1,26 @@
 // LeetCode - 322. Coin Change
-// You are given an integer array [coins] representing coins of different denominations and integer [amount]representing a total amount of money.
-// Return the fewest number of coins that you need ot make up that amount. If that amount of money cannont be made up by any combination of the coins, return -1.
-// You may assume that you have an infinite number of each kind of coin
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+
+// Dynamic programming problem. Uses a method called tabulation to solve it.
+// Any time you see tabulation you want to think 'an array' and 'iteration.'
+
+let coinChange = (coins, amount) => {
+  const table = new Array(amount + 1).fill(Infinity);
+  table[0] = 0;
+  for (let coin of coins) {
+    for (let i = 0; i < table.length; i++) {
+      if (coin <= i) {
+        let idx = i - coin;
+        let potentialAmt = table[idx] + 1;
+        table[i] = Math.min(potentialAmt, table[i]);
+      }
+    }
+  }
+  return table[table.length - 1] === Infinity ? -1 : table[table.length - 1];
+};
+
+console.log(coinChange([1, 2, 5], 11));
