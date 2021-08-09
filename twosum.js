cb23f85fbea0b0c906e8fc2c80b645329581ef68
Joe Zucco <josephzucco@gmail.com>
2021-08-08 22:02:34 -0400

    solution

--- twosum.js
+++ twosum.js
@@ -6,13 +6,17 @@
  */
 
 let twoSum = (nums, target) => {
-    const previousValues = {}
-    for (let i = 0; i < nums.length; i++){
-        const currentNumber = nums[i]
-        const neededValues = target - currentNumber
-        const index2 = previousValues[neededValue]
-        if (index2 != null) {
-            return [index2, i]
-        }
+  const previousValues = {};
+  for (let i = 0; i < nums.length; i++) {
+    const currentNumber = nums[i];
+    const neededValue = target - currentNumber;
+    const index2 = previousValues[neededValue];
+    if (index2 != null) {
+      return [index2, i];
+    } else {
+      previousValues[currentNumber] = i;
     }
+  }
 };
+
+console.log(twoSum([2, 8, 3, 6], 10));
