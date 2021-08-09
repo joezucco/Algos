2df49c98a759d1d5e175079b59683a8928c61b4b
Joe Zucco <josephzucco@gmail.com>
2021-08-08 20:54:28 -0400

    coin change init

---
+++ coinchange.js
@@ -0,0 +1,4 @@
+// LeetCode - 322. Coin Change
+// You are given an integer array [coins] representing coins of different denominations and integer [amount]representing a total amount of money.
+// Return the fewest number of coins that you need ot make up that amount. If that amount of money cannont be made up by any combination of the coins, return -1.
+// You may assume that you have an infinite number of each kind of coin
