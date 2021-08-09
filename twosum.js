839e93f552ee1e6dfbd2c748bc6ac8c4f721f429
Joe Zucco <josephzucco@gmail.com>
2021-08-08 21:59:55 -0400

    two sum init

---
+++ twosum.js
@@ -0,0 +1,18 @@
+// LeetCode - 1. Two Sum
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+let twoSum = (nums, target) => {
+    const previousValues = {}
+    for (let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i]
+        const neededValues = target - currentNumber
+        const index2 = previousValues[neededValue]
+        if (index2 != null) {
+            return [index2, i]
+        }
+    }
+};
