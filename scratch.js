b5cefa73fbe6133a0bf3816aaac9a1d16c9e0b24
Joe Zucco <josephzucco@gmail.com>
2021-08-10 15:46:01 -0400

    coderbyte questions

---
+++ scratch.js
@@ -0,0 +1,13 @@
+// Coderbyte Basic JS Multiple Choice
+for (let i = 0; i < 3; i++) {
+  setTimeout(function () {
+    alert(i);
+  }, 1000 + i);
+}
+console.log(setTimeout());
+
+(function () {
+    var a = b = 5;
+})()
+
+console.log(b);
