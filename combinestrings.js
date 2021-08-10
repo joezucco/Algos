df69a17525074d201940c5d73f28c7e8abad5dfe
Joe Zucco <josephzucco@gmail.com>
2021-08-10 09:37:13 -0400

    concat method or spread operator

--- combinestrings.js
+++ combinestrings.js
@@ -1,3 +1,5 @@
 // Codewars - Grasshopper - Combine strings
-const combineNames = (first, last) => { first.concat(last) }
-console.log(combineNames('Joe','Zucco'));
+const combineNames = (first, last) => first.concat(" " + last);
+//or
+const combineNames = (...names) => names.join(" ");
+console.log(combineNames("Joe", "Zucco"));
